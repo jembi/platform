711b413ca101f96a0543644b31d10a192b06b74d
MattyJ007 <matthew.dickie@jembi.org>
2022-01-10 11:37:21 +0200

    Improve swarm elastic stack scripts
    Add dev and prod deploy modes
    Fix config file setup
    Remove metric beats from pipeline and analytics (this has its own
    package now)
    
    PLAT-15

--- elastic-analytics/swarm.sh
+++ elastic-analytics/swarm.sh
@@ -3,11 +3,13 @@
 composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 if [ "$1" == "init" ]; then
-    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.stack-0.yml instant
+  apt-get install -y expect >/dev/null 2>&1
+  
+  if  [ "$2" == "dev" ]; then
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.dev.yml -c "$composeFilePath"/docker-compose.stack-0.yml instant
 
     echo "Waiting for elasticsearch to start before automatically setting built-in passwords..."
     sleep 40
-    apt-get install -y expect >/dev/null 2>&1
     echo "Setting passwords..."
     "$composeFilePath"/set-pwds-swarm.exp
     echo "Done"
@@ -17,17 +19,35 @@ if [ "$1" == "init" ]; then
     sed -i "s/dev_password_only/$ES_ELASTIC/g" "$composeFilePath"/../../elastic-pipeline/logstash.yml
     echo "Done"
 
-    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.stack-1.yml instant
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.dev.yml -c "$composeFilePath"/docker-compose.stack-1.yml instant
+  else
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.prod.yml -c "$composeFilePath"/docker-compose.stack-0.yml instant
+
+    echo "Waiting for elasticsearch to start before automatically setting built-in passwords..."
+    sleep 40
+    echo "Setting passwords..."
+    "$composeFilePath"/set-pwds-swarm.exp
+    echo "Done"
+
+    echo "Setting passwords in config files..."
+    sed -i "s/dev_password_only/$ES_ELASTIC/g" "$composeFilePath"/../../elastic-pipeline/pipeline/*.conf
+    sed -i "s/dev_password_only/$ES_ELASTIC/g" "$composeFilePath"/../../elastic-pipeline/logstash.yml
+    echo "Done"
+
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.prod.yml -c "$composeFilePath"/docker-compose.stack-1.yml instant
+  fi
 elif [ "$1" == "up" ]; then
-    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.stack-1.yml instant
+  if  [ "$2" == "dev" ]; then
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.dev.yml -c "$composeFilePath"/docker-compose.stack-1.yml instant
+  else
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.prod.yml -c "$composeFilePath"/docker-compose.stack-1.yml instant
+  fi
 elif [ "$1" == "down" ]; then
-    docker service scale instant_es-analytics=0 instant_kibana=0 instant_jsreport=0
-    docker service rm instant_metricbeat
+    docker service scale instant_es-analytics=0 instant_kibana=0
 elif [ "$1" == "destroy" ]; then
-    docker service rm instant_es-analytics instant_kibana instant_jsreport instant_metricbeat
+    docker service rm instant_es-analytics instant_kibana
     sleep 15
-    # TODO - Need to for this for all nodes...
-    docker volume rm instant_es-data instant_metricbeat-data01
+    docker volume rm instant_es-data
 else
     echo "Valid options are: init, up, down, or destroy"
 fi
--- elastic-pipeline/swarm.sh
+++ elastic-pipeline/swarm.sh
@@ -3,15 +3,21 @@
 composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 if [ "$1" == "init" ]; then
-    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.stack.yml instant
+    if  [ "$2" == "dev" ]; then
+        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.dev.yml instant
+    else
+        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.prod.yml instant
+    fi
 elif [ "$1" == "up" ]; then
-    docker stack deploy -c "$composeFilePath"/docker-compose.yml instant
+    if  [ "$2" == "dev" ]; then
+        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.dev.yml instant
+    else
+        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.prod.yml instant
+    fi
 elif [ "$1" == "down" ]; then
-    docker service scale instant_logstash=0 instant_reprocess-mediator=0
+    docker service scale instant_logstash=0
 elif [ "$1" == "destroy" ]; then
-    docker service rm instant_logstash instant_reprocess-mediator
-    sleep 15
-    docker volume rm logstash-pipeline logstash-config
+    docker service rm instant_logstash
 else
     echo "Valid options are: init, up, down, or destroy"
 fi
