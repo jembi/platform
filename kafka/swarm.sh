0ac2c50d110b90b126b47667d0c2004ddaaa53e3
bradsawadye <bradsawadye@gmail.com>
2022-01-10 15:28:10 +0200

    Add the swarm scripts for the kafka package
    These are the scripts for deploying the kafka service to a docker swarm
    
    PLAT-8

---
+++ kafka/swarm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+if [ "$1" == "init" ]; then
+    if  [ "$2" == "dev" ]; then
+        docker stack deploy -c "$composeFilePath"/docker-compose.yml instant
+        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.stack.yml instant
+    else
+        docker stack deploy -c "$composeFilePath"/docker-compose.yml instant
+        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.prod.yml  -c "$composeFilePath"/docker-compose.stack.yml instant
+    fi
+elif [ "$1" == "up" ]; then
+    if  [ "$2" == "dev" ]; then
+        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.stack.yml instant
+    else
+        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.prod.yml  -c "$composeFilePath"/docker-compose.stack.yml instant
+    fi
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.stack.yml instant
+elif [ "$1" == "down" ]; then
+    docker service scale instant_zookeeper=0 instant_kafka=0 instant_kafdrop=0
+elif [ "$1" == "destroy" ]; then
+    docker service rm instant_zookeeper instant_kafka instant_kafdrop
+
+    echo "Sleep 10 Seconds to allow services to shut down before deleting volumes"
+    sleep 10
+
+    docker volume rm instant_kafka-volume
+else
+    echo "Valid options are: init, up, down, or destroy"
+fi
