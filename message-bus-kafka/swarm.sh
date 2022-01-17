61eb63616b26c5e94512016c928669d7d94885a4
MattyJ007 <matthew.dickie@jembi.org>
2022-01-17 12:26:22 +0200

    Lint message bus swarm file
    The newline spacing was not consistent with the project (2 speace)
    
    PLAT-34

--- message-bus-kafka/swarm.sh
+++ message-bus-kafka/swarm.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
-composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+composeFilePath=$(
+  cd "$(dirname "${BASH_SOURCE[0]}")"
+  pwd -P
+)
 
 if [ "$1" == "init" ]; then
-    if  [ "$2" == "dev" ]; then
-        docker stack deploy -c "$composeFilePath"/docker-compose.yml instant
-        sleep 30
-        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.stack.yml instant
-    else
-        docker stack deploy -c "$composeFilePath"/docker-compose.yml instant
-        sleep 30
-        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.prod.yml -c "$composeFilePath"/docker-compose.stack.yml instant
-    fi
+  if [ "$2" == "dev" ]; then
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml instant
+    sleep 30
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.stack.yml instant
+  else
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml instant
+    sleep 30
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.prod.yml -c "$composeFilePath"/docker-compose.stack.yml instant
+  fi
 elif [ "$1" == "up" ]; then
-    if  [ "$2" == "dev" ]; then
-        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.stack.yml instant
-    else
-        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.prod.yml -c "$composeFilePath"/docker-compose.stack.yml instant
-    fi
+  if [ "$2" == "dev" ]; then
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.stack.yml instant
+  else
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.prod.yml -c "$composeFilePath"/docker-compose.stack.yml instant
+  fi
 elif [ "$1" == "down" ]; then
-    docker service scale instant_zookeeper=0 instant_kafka=0 instant_kafdrop=0
+  docker service scale instant_zookeeper=0 instant_kafka=0 instant_kafdrop=0
 elif [ "$1" == "destroy" ]; then
-    docker service rm instant_zookeeper instant_kafka instant_kafdrop
+  docker service rm instant_zookeeper instant_kafka instant_kafdrop
 
-    echo "Sleep 20 Seconds to allow services to shut down before deleting volumes"
-    sleep 20
+  echo "Sleep 20 Seconds to allow services to shut down before deleting volumes"
+  sleep 20
 
-    docker volume rm instant_kafka-volume
+  docker volume rm instant_kafka-volume
 else
-    echo "Valid options are: init, up, down, or destroy"
+  echo "Valid options are: init, up, down, or destroy"
 fi
