cc095c75b86dc55fe6f15382fdaa00a70cd7f9f9
Matt <matthew.dickie@jembi.org>
2022-01-27 12:00:55 +0200

    Update docs/docusaurus.config.js
    PLAT-18
    
    Co-authored-by: Michael Loosen <24909683+michaelloosen@users.noreply.github.com>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -36,7 +36,7 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/jembi/platform/tree/master/docs/'
+          editUrl: 'https://github.com/jembi/platform/tree/main/docs/'
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css')
