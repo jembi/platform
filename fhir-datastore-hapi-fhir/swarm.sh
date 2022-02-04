7b391fd99388e0215ea5aeb111b464bcb9a24d36
MattyJ007 <matthew.dickie@jembi.org>
2022-02-04 08:43:40 +0200

    Add cluster logic for hapi fhir service
    Add replica set for postgres.
    Working as a replica but there is an issue with lead postgres node after
    re-election
    
    PLAT-78

--- fhir-datastore-hapi-fhir/swarm.sh
+++ fhir-datastore-hapi-fhir/swarm.sh
@@ -4,39 +4,39 @@ composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 if [ "$1" == "init" ]; then
     if  [ "$2" == "dev" ]; then
-        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.dev.yml -c "$composeFilePath"/docker-compose.stack-0.yml instant
+        docker stack deploy -c "$composeFilePath"/docker-compose-postgres.yml -c "$composeFilePath"/docker-compose-postgres.dev.yml instant
 
-        echo "Sleep 60 seconds to give Postgres time to start up before HAPI-FHIR run"
+        echo "Sleep 60 seconds to give Postgres time to start up before HAPI-FHIR"
         sleep 60
 
-        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.dev.yml -c "$composeFilePath"/docker-compose.stack-1.yml instant
+        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.dev.yml instant
     else
-        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.prod.yml -c "$composeFilePath"/docker-compose.stack-0.yml instant
+        docker stack deploy -c "$composeFilePath"/docker-compose-postgres.yml -c "$composeFilePath"/docker-compose-postgres.cluster.yml instant
 
-        echo "Sleep 60 seconds to give Postgres time to start up before HAPI-FHIR run"
+        echo "Sleep 60 seconds to give Postgres time to start up before HAPI-FHIR"
         sleep 60
 
-        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.prod.yml -c "$composeFilePath"/docker-compose.stack-1.yml instant
+        docker stack deploy -c "$composeFilePath"/docker-compose.yml instant
     fi
 elif [ "$1" == "up" ]; then
     if [ "$2" == "dev" ]; then
-        docker stack deploy -c "$composeFilePath"/docker-compose.mongo.yml -c "$composeFilePath"/docker-compose.mongo.dev.yml instant
+        docker stack deploy -c "$composeFilePath"/docker-compose-postgres.yml -c "$composeFilePath"/docker-compose-postgres.dev.yml instant
         sleep 20
-        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.dev.yml -c "$composeFilePath"/docker-compose.stack-1.yml instant
+        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.dev.yml instant
     else
-        docker stack deploy -c "$composeFilePath"/docker-compose.mongo.yml instant
+        docker stack deploy -c "$composeFilePath"/docker-compose-postgres.yml -c "$composeFilePath"/docker-compose-postgres.cluster.yml instant
         sleep 20
-        docker stack deploy -c "$composeFilePath"/docker-compose.yml -c "$composeFilePath"/docker-compose.stack-1.yml instant
+        docker stack deploy -c "$composeFilePath"/docker-compose.yml instant
     fi 
 elif [ "$1" == "down" ]; then
-    docker service scale instant_hapi-fhir=0 instant_hapi-db=0
+    docker service scale instant_hapi-fhir=0 instant_postgres-1=0 instant_postgres-2=0 instant_postgres-3=0
 elif [ "$1" == "destroy" ]; then
-    docker service rm instant_hapi-fhir instant_hapi-db
+    docker service rm instant_hapi-fhir instant_postgres-1 instant_postgres-2 instant_postgres-3
 
     echo "Sleep 10 Seconds to allow services to shut down before deleting volumes"
     sleep 10
 
-    docker volume rm instant_hapi-db-volume
+    docker volume rm hapi-postgres1 hapi-postgres2 hapi-postgres3
 else
     echo "Valid options are: init, up, down, or destroy"
 fi
