c2ee10f4fa5b027b2957985f2798ece04e74d3fb
bradsawadye <bradsawadye@gmail.com>
2022-02-10 15:38:21 +0200

    Correct the volume names
    In a docker stack the volume name is changed. The stack's name is prepended to the named volume
    
    PLAT-26

--- fhir-datastore-hapi-fhir/swarm.sh
+++ fhir-datastore-hapi-fhir/swarm.sh
@@ -47,7 +47,7 @@ elif [ "$1" == "destroy" ]; then
   echo "Sleep 10 Seconds to allow services to shut down before deleting volumes"
   sleep 10
 
-  docker volume rm hapi-postgres-1-data hapi-postgres-2-data hapi-postgres-3-data
+  docker volume rm instant_hapi-postgres-1-data instant_hapi-postgres-2-data instant_hapi-postgres-3-data
 else
   echo "Valid options are: init, up, down, or destroy"
 fi
