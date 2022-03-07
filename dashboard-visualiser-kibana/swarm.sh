00ce7a4ea1f84b2df7ca1eddc65e4b7bc8f8f33f
Castello Govender <castello.govender@jembi.org>
2022-03-07 14:26:45 +0200

    Added swarm.sh file for kibana dashboard package To allow the package to be deployed

---
+++ dashboard-visualiser-kibana/swarm.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+statefulNodes=${STATEFUL_NODES:-"cluster"}
+
+composeFilePath=$(
+  cd "$(dirname "${BASH_SOURCE[0]}")"
+  pwd -P
+)
+
+if [ $statefulNodes == "cluster" ]; then
+  printf "\nRunning Dashboard Visualiser Kibana package in Cluster node mode\n"
+  kibanaClusterComposeParam="-c ${composeFilePath}/docker-compose.cluster.yml"
+else
+  printf "\nRunning Dashboard Visualiser Kibana package in Single node mode\n"
+  kibanaClusterComposeParam=""
+fi
+
+if [ "$2" == "dev" ]; then
+  printf "\nRunning Dashboard Visualiser Kibana package in DEV mode\n"
+  kibanaDevComposeParam="-c ${composeFilePath}/docker-compose.dev.yml"
+else
+  printf "\nRunning Dashboard Visualiser Kibana package in PROD mode\n"
+  kibanaDevComposeParam=""
+fi
+
+if [ "$1" == "init"] || ["$1" == "up" ]; then
+  docker stack deploy -c "$composeFilePath"/docker-compose.yml $kibanaClusterComposeParam $kibanaDevComposeParam instant
+elif [ "$1" == "down" ]; then
+  docker service scale instant_kibana=0
+elif [ "$1" == "destroy" ]; then
+  docker service rm instant_kibana
+else
+  echo "Valid options are: init, up, down, or destroy"
+fi
