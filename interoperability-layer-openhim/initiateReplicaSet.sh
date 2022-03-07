0162339fb616e606017558eca3c8d32b26c4b442
mark-labuschagne <marklabuschagne7@gmail.com>
2022-03-07 15:57:49 +0200

    Replace hardcoded sleeps with checks in initiateReplicaSet.sh To ensure that the replica sets are initiated

--- interoperability-layer-openhim/initiateReplicaSet.sh
+++ interoperability-layer-openhim/initiateReplicaSet.sh
@@ -7,8 +7,20 @@ echo 'Initiating the mongo replica set'
 config='{"_id":"mongo-set","members":[{"_id":0,"priority":1,"host":"mongo-1:27017"},
 {"_id":1,"priority":0.5,"host":"mongo-2:27017"},{"_id":2,"priority":0.5,"host":"mongo-3:27017"}]}'
 
-# Sleep to ensure all the mongo instances for the replica set are up and running
-sleep 50
+echo '\nSleep to ensure all the mongo instances for the replica set are up and running'
+runningInstanceCount="0"
+while [ $runningInstanceCount != "3" ]
+do
+    runningInstanceCount="0"
+    for i in $(docker service ls -f name=instant_mongo)
+    do
+        if [ $i = "1/1" ]; then
+            runningInstanceCount=`expr $runningInstanceCount + 1`   
+        fi
+    done
+done
+# This sleep ensures that the replica sets are reachable
+sleep 5
 
 containerName='mongo-1'
 
