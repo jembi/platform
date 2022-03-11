c33676b71f88bccef5b7309a9f9aef2ee381400a
Castello Govender <castello.govender@jembi.org>
2022-03-11 11:30:50 +0200

    Renamed set-pwds to set-elastic-passwords Also added more documentation to this script So that we may more easily understand it's re requirements and what its for

--- analytics-datastore-elastic-search/swarm.sh
+++ analytics-datastore-elastic-search/swarm.sh
@@ -25,14 +25,16 @@ fi
 
 if [ "$1" == "init" ]; then
   docker stack deploy -c "$composeFilePath"/docker-compose.yml $elasticSearchClusterComposeParam $elasticSearchDevComposeParam instant
+
   echo "Waiting for elasticsearch to start before automatically setting built-in passwords..."
   sleep 40
   # >/dev/null 2>&1 throws all terminal input and output text away
   apt-get install -y expect >/dev/null 2>&1
   echo "Setting passwords..."
   elasticSearchContainerId=$(docker ps -f name=instant_analytics-datastore-elastic-search --format "{{.ID}}")
-  "$composeFilePath"/set-pwds.exp $elasticSearchContainerId > /dev/null 2>&1
+  "$composeFilePath"/set-elastic-passwords.exp $elasticSearchContainerId > /dev/null 2>&1
   echo "Done"
+
 elif [ "$1" == "up" ]; then
   docker stack deploy -c "$composeFilePath"/docker-compose.yml $elasticSearchClusterComposeParam $elasticSearchDevComposeParam instant
 elif [ "$1" == "down" ]; then
@@ -44,7 +46,6 @@ elif [ "$1" == "destroy" ]; then
   sleep 20
 
   docker volume rm instant_es-data
-  # do we need to rm -r the physical data binding /backups/elasticsearch ??
 
   if [ $statefulNodes == "cluster" ]; then
     echo "Volumes are only deleted on the host on which the command is run. Elastic Search volumes on other nodes are not deleted"
