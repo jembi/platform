598d68d450dd9f24d77f388525e96c4e4d698578
bradsawadye <bradsawadye@gmail.com>
2022-03-14 13:25:53 +0200

    Add hapi fhir helper package
    This is a package for importing fhir igs to validate against
    
    PLAT-159

---
+++ fhir-datastore-helper-hapi-fhir/swarm.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+hapiFhirReplicas=${HAPI_FHIR_INSTANCES:-1}
+fhirIG=${FHIR_IG_URL}
+
+composeFilePath=$(
+  cd "$(dirname "${BASH_SOURCE[0]}")"
+  pwd -P
+)
+
+if [ -n "$fhirIgUrl" ]; then
+  echo "FHIR IG url should be specified using the env variable 'FHIR_IG_URL'"
+else
+  if [ "$1" == "init" ]; then
+    docker stack deploy -c "$composeFilePath"/docker-compose.yml instant
+
+    echo "Sleep 60 seconds to allow the FHIR IG import processs to finish"
+    sleep 60
+
+    docker service scale instant_hapi-fhir=0
+
+    echo "Sleep 10 seconds to give HAPI-FHIR time to scale down"
+    sleep 10
+
+    docker service scale instant_hapi-fhir=$hapiFhirReplicas
+    docker service rm instant_hapi-fhir-config-importer
+
+    # Sleep to allow the config importer to be removed. This is to enable the removal of the instant volume
+    sleep 120
+  else
+    echo "Valid option is init"
+  fi
+fi
