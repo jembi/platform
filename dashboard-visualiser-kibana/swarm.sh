a49055850c2ac6b7881280282a7e94517a0b79d9
Castello Govender <castello.govender@jembi.org>
2022-03-24 11:26:21 +0200

    Removed stale cluster configs for kibana To ensure the file only contains relevant code https://github.com/jembi/platform/pull/33#discussion_r833372723 https://github.com/jembi/platform/pull/33#discussion_r833372451 https://github.com/jembi/platform/pull/33#discussion_r833372983

--- dashboard-visualiser-kibana/swarm.sh
+++ dashboard-visualiser-kibana/swarm.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-statefulNodes=${STATEFUL_NODES:-"cluster"}
-
 composeFilePath=$(
   cd "$(dirname "${BASH_SOURCE[0]}")"
   pwd -P
 )
-
-if [ $statefulNodes == "cluster" ]; then
-  printf "\nRunning Dashboard Visualiser Kibana package in Cluster node mode\n"
-  kibanaClusterComposeParam="-c ${composeFilePath}/docker-compose.cluster.yml"
-else
-  printf "\nRunning Dashboard Visualiser Kibana package in Single node mode\n"
-  kibanaClusterComposeParam=""
-fi
 
 if [ "$2" == "dev" ]; then
   printf "\nRunning Dashboard Visualiser Kibana package in DEV mode\n"
@@ -24,7 +14,7 @@ else
 fi
 
 if [ "$1" == "init" ] || [ "$1" == "up" ]; then
-  docker stack deploy -c "$composeFilePath"/docker-compose.yml $kibanaClusterComposeParam $kibanaDevComposeParam instant
+  docker stack deploy -c "$composeFilePath"/docker-compose.yml $kibanaDevComposeParam instant
 elif [ "$1" == "down" ]; then
   docker service scale instant_dashboard-visualiser-kibana=0
 elif [ "$1" == "destroy" ]; then
