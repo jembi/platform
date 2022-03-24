15928133d11147098a60ba0dfe89bfb2db91d2b4
Castello Govender <castello.govender@jembi.org>
2022-03-24 10:19:38 +0200

    Added fail checks to each es init action To stop the script when anything fails So that the script does not get stuck Also moved each action into its own function To make the init portion more readable
    https://github.com/jembi/platform/pull/33#discussion_r832137572
    https://github.com/jembi/platform/pull/33#discussion_r832156156
    https://github.com/jembi/platform/pull/33#discussion_r832191584
    https://github.com/jembi/platform/pull/33#discussion_r832193704
    https://github.com/jembi/platform/pull/33#discussion_r832194111

--- analytics-datastore-elastic-search/swarm.sh
+++ analytics-datastore-elastic-search/swarm.sh
@@ -7,7 +7,97 @@ composeFilePath=$(
   pwd -P
 )
 
-if [ $statefulNodes == "cluster" ]; then 
+function awaitContainerStartup() {
+  echo "Waiting for elasticsearch container to start up..."
+
+  local warningTime=60
+  local errorTime=300
+  local timer=0
+
+  until [[ -n $(docker ps -qlf name=instant_analytics-datastore-elastic-search) ]]; 
+  do 
+      if [ $timer == $warningTime ]; then
+          echo "Warning: container is taking unusually long to start"
+      fi;
+      if [ $timer == $errorTime ]; then
+          echo "Fatal: Elasticsearch container took too long to start up"
+          exit 124; # exit code for timeout is 124
+      fi;
+      sleep 1; 
+      timer=$((timer+1))
+  done;
+  echo "Elasticsearch container started up"
+}
+
+function awaitContainerReady() {
+  echo "Waiting for elasticsearch container to be in ready state..."
+
+  local warningTime=60
+  local errorTime=300
+  local timer=0
+
+  until [[ "$(docker inspect -f '{{.State.Status}}' $(docker ps -qlf name=instant_analytics-datastore-elastic-search))" = "running" ]]; 
+  do 
+      if [ $timer == $warningTime ]; then
+          echo "Warning: container is taking unusually long to start"
+      fi;
+      if [ $timer == $errorTime ]; then
+          echo "Fatal: Elasticsearch container took too long to start up"
+          exit 124; # exit code for timeout is 124
+      fi;
+      sleep 1; 
+      timer=$((timer+1))
+  done;
+  echo "Elasticsearch container is in ready state"
+}
+
+function installExpect() {
+  echo "Installing Expect..."
+  # >/dev/null 2>&1 throws all terminal input and output text away
+  apt-get install -y expect >/dev/null 2>&1
+  if [ $? -eq 1 ]
+  then
+    echo "Fatal: Failed to install Expect library. Cannot update Elastic Search passwords"
+    exit 1
+  fi
+  echo "Done installing Expect"
+}
+
+function setElasticsearchPasswords() {
+  echo "Setting passwords..."
+  elasticSearchContainerId=$(docker ps -qlf name=instant_analytics-datastore-elastic-search)
+  "$composeFilePath"/set-elastic-passwords.exp "$elasticSearchContainerId" >/dev/null 2>&1
+  if [ $? -eq 1 ]
+  then
+    echo "Fatal: Failed to set elastic passwords. Cannot update Elastic Search passwords"
+    exit 1
+  fi
+  echo "Passwords set"
+}
+
+function awaitContainerDestroy() {
+  echo "Waiting for elasticsearch container to be destroyed"
+
+  local warningTime=60
+  local errorTime=300
+  local timer=0
+
+  until [[ -z $(docker ps -qlf name=instant_analytics-datastore-elastic-search) ]]; 
+  do 
+      if [ $timer == $warningTime ]; then
+          echo "Warning: container is taking unusually long to be destroyed"
+      fi;
+      if [ $timer == $errorTime ]; then
+          echo "Fatal: Elasticsearch container took too long to be destroyed"
+          exit 124; # exit code for timeout is 124
+      fi;
+      sleep 1; 
+      timer=$((timer+1))
+  done;
+  echo "Elasticsearch container is destroyed"
+}
+
+if [ "$statefulNodes" == "cluster" ]; then 
   printf "\nRunning Analytics Datastore Elastic Search package in Cluster node mode\n"
   elasticSearchClusterComposeParam="-c ${composeFilePath}/docker-compose.cluster.yml"
 else
@@ -27,18 +117,13 @@ if [ "$1" == "init" ]; then
   docker stack deploy -c "$composeFilePath"/docker-compose.yml $elasticSearchClusterComposeParam $elasticSearchDevComposeParam instant
 
   echo "Waiting for elasticsearch to start before automatically setting built-in passwords..."
-  echo "Waiting for elasticsearch container to start up"
-  until [[ ! -z $(docker ps -qlf name=instant_analytics-datastore-elastic-search) ]]; do sleep 0.25; done;
-  echo "Waiting for elasticsearch container to be in ready state"
-  until [[ "$(docker inspect -f '{{.State.Status}}' $(docker ps -qlf name=instant_analytics-datastore-elastic-search))" = "running" ]]; do sleep 0.25; done
+  awaitContainerStartup
+  awaitContainerReady
 
-  # >/dev/null 2>&1 throws all terminal input and output text away
-  apt-get install -y expect >/dev/null 2>&1
-  echo "Setting passwords..."
-  elasticSearchContainerId=$(docker ps -qlf name=instant_analytics-datastore-elastic-search)
-  "$composeFilePath"/set-elastic-passwords.exp $elasticSearchContainerId >/dev/null 2>&1
-  echo "Done"
-
+  installExpect
+  setElasticsearchPasswords
+  
+  echo "Done initialising"
 elif [ "$1" == "up" ]; then
   docker stack deploy -c "$composeFilePath"/docker-compose.yml $elasticSearchClusterComposeParam $elasticSearchDevComposeParam instant
 elif [ "$1" == "down" ]; then
@@ -46,14 +131,13 @@ elif [ "$1" == "down" ]; then
 elif [ "$1" == "destroy" ]; then
   docker service rm instant_analytics-datastore-elastic-search
 
-  echo "Waiting for services to shut down before deleting volumes"
-  until [[ -z $(docker ps -qlf name=instant_analytics-datastore-elastic-search) ]]; do sleep 0.25; done;
-
+  awaitContainerDestroy
+  
   docker volume rm instant_es-data
 
-  if [ $statefulNodes == "cluster" ]; then
+  if [ "$statefulNodes" == "cluster" ]; then
     echo "Volumes are only deleted on the host on which the command is run. Elastic Search volumes on other nodes are not deleted"
   fi
-else
+elsehttps://stackoverflow.com/questions/69252733/bash-local-variable-scope-best-practice
   echo "Valid options are: init, up, down, or destroy"
 fi
