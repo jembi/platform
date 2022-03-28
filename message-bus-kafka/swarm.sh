2b7aceb07bb1a52fc2ea4f1b15584e3100f78280
Ryan Crichton <ryan@jembi.org>
2022-03-28 15:09:36 +0200

    Add zookeeper clustering
    This is statically set to 3 in cluster mode, we will need templating of compose files to make this dynamic.

--- message-bus-kafka/swarm.sh
+++ message-bus-kafka/swarm.sh
@@ -26,19 +26,28 @@ fi
 if [ "$1" == "init" ]; then
   docker stack deploy -c "$composeFilePath"/docker-compose.yml $kafkaClusterComposeParam $kafkaDevComposeParam instant
 elif [ "$1" == "up" ]; then
-  docker stack deploy -c "$composeFilePath"/docker-compose.yml -c $kafkaClusterComposeParam $kafkaDevComposeParam instant
+  docker stack deploy -c "$composeFilePath"/docker-compose.yml $kafkaClusterComposeParam $kafkaDevComposeParam instant
 elif [ "$1" == "down" ]; then
-  docker service scale instant_zookeeper=0 instant_kafka=0 instant_kafdrop=0 
+  docker service scale instant_zookeeper-1=0 instant_kafdrop=0
+  # You cannot scale a global service so we have to remove it
+  docker service rm instant_kafka
+  if [ $statefulNodes == "cluster" ]; then
+    docker service scale instant_zookeeper-2=0
+    docker service scale instant_zookeeper-3=0
+  fi
 elif [ "$1" == "destroy" ]; then
-  docker service rm instant_zookeeper instant_kafka instant_kafdrop
+  docker service rm instant_zookeeper-1 instant_kafka instant_kafdrop
 
   echo "Sleep 20 Seconds to allow services to shut down before deleting volumes"
   sleep 20
 
   # TODO remove from all nodes
   docker volume rm instant_kafka-volume
+  docker volume rm instant_zookeeper-1-volume
 
   if [ $statefulNodes == "cluster" ]; then
+    docker service rm instant_zookeeper-2
+    docker service rm instant_zookeeper-3
     echo "Volumes are only deleted on the host on which the command is run. Kafka volumes on other nodes are not deleted"
   fi
 else
