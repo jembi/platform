83ff00c7bdf0328f6af4259be60e85054fbdb24c
mark-labuschagne <marklabuschagne7@gmail.com>
2022-04-04 17:22:32 +0200

    Introduce cluster compose file and relevant swarm.sh changes So that jsreport runs with the necessary constraints and volumes when operating in a clustered system. PLAT-194

--- dashboard-visualiser-jsreport/swarm.sh
+++ dashboard-visualiser-jsreport/swarm.sh
@@ -2,27 +2,36 @@
 
 STATEFUL_NODES=${STATEFUL_NODES:-"cluster"}
 
-ComposeFilePath=$(
+COMPOSE_FILE_PATH=$(
   cd "$(dirname "${BASH_SOURCE[0]}")" || exit
   pwd -P
 )
 
+if [[ "$STATEFUL_NODES" == "cluster" ]]; then
+  printf "\nRunning Analytics Datastore Elastic Search package in Cluster node mode\n"
+  JsReportsClusterComposeParam="-c ${COMPOSE_FILE_PATH}/docker-compose.cluster.yml"
+else
+  printf "\nRunning Analytics Datastore Elastic Search package in Single node mode\n"
+  JsReportsClusterComposeParam=""
+fi
+
 if [[ "$2" == "dev" ]]; then
   printf "\nRunning JS Reports package in DEV mode\n"
-  JsReportDevComposeParam="-c ${ComposeFilePath}/docker-compose.dev.yml"
+  JsReportDevComposeParam="-c ${COMPOSE_FILE_PATH}/docker-compose.dev.yml"
 else
   printf "\nRunning JS Reports package in PROD mode\n"
   JsReportDevComposeParam=""
 fi
 
 if [[ "$1" == "init" ]]; then
-  docker stack deploy -c "$ComposeFilePath"/docker-compose.yml $JsReportDevComposeParam instant
+  docker stack deploy -c "$COMPOSE_FILE_PATH"/docker-compose.yml $JsReportDevComposeParam $JsReportsClusterComposeParam instant
 elif [[ "$1" == "up" ]]; then
-  docker stack deploy -c "$ComposeFilePath"/docker-compose.yml $JsReportDevComposeParam instant
+  docker stack deploy -c "$COMPOSE_FILE_PATH"/docker-compose.yml $JsReportDevComposeParam $JsReportsClusterComposeParam instant
 elif [[ "$1" == "down" ]]; then
   docker service scale instant_dashboard-visualiser-jsreport=0
 elif [[ "$1" == "destroy" ]]; then
   docker service rm instant_dashboard-visualiser-jsreport
+  docker config rm instant_jsreport-export.jsrexport
 else
   echo "Valid options are: init, up, down, or destroy"
 fi
