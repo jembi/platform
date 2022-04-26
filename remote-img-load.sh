76c2517fea1b25c1fe2b8410be7869e7830294bc
Ryan Crichton <ryan@jembi.org>
2022-04-26 15:51:59 +0200

    Fix style

--- remote-img-load.sh
+++ remote-img-load.sh
@@ -7,5 +7,5 @@ fi
 
 ./build-image.sh
 echo "Transfering image to $1 ..."
-docker save jembi/platform:latest | bzip2 | ssh ubuntu@$1 docker load
+docker save jembi/platform:latest | bzip2 | ssh ubuntu@"$1" docker load
 echo "Image transfered"
