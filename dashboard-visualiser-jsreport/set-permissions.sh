490ad5d954d66b2de80fb3e7acea3563055d2bc2
mark-labuschagne <marklabuschagne7@gmail.com>
2022-05-24 12:08:28 +0200

    Add description to reset-permissions.sh script
    PLAT-237

--- dashboard-visualiser-jsreport/set-permissions.sh
+++ dashboard-visualiser-jsreport/set-permissions.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script is here to set and reset the file permissions that might have been altered when running
+# JS Report with the dev mount attached. Make sure that you run this script before and after running JS Report
+# with the dev mount attached.
+
 if [[ $(basename $(pwd)) != "dashboard-visualiser-jsreport" ]]; then
     echo "Please change your working directory to dashboard-visualiser-jsreport's root directory before running the script. Exiting..."
     exit 1
