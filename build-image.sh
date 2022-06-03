636a49b880ede24668ae46528b6dea87746b6663
mark-labuschagne <marklabuschagne7@gmail.com>
2022-06-03 16:14:32 +0200

    Update build-image.sh to build go scripts
    POC-go-scripting

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
+packages=(
+    "dashboard-visualiser-jsreport"
+    "dashboard-visualiser-kibana"
+)
+
+build_go_binary() {
+    for ((i = 0; i < ${#packages[@]}; i++)); do
+        cd ${packages[$i]}
+        GOOS=linux GOARCH=amd64 go build -o swarm
+        cd ..
+    done
+}
+
+remove_go_binary() {
+    for ((i = 0; i < ${#packages[@]}; i++)); do
+        local package=${packages[$i]}
+        cd "$package"
+        rm -f "swarm"
+        cd ..
+    done
+}
+
+build_go_binary $packages
+
 docker build -t jembi/platform:latest .
+
+remove_go_binary $packages
