a076b18cb0a8320bdeb6273a8e18a5136c552e18
michaelloosen <thorne199@gmail.com>
2022-06-15 11:19:43 +0200

    Added functionality to get-cli to split downloads To ensure the user of the script can download only the binary that they require. Also to allow the version to be specified as a parameter to the script

--- get-cli.sh
+++ get-cli.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
 
-cliVersion=0.1.0
+option="${1:-all}"
+cliVersion=${2:-0.1.0}
 
-curl -L https://github.com/openhie/package-starter-kit/releases/download/$cliVersion/gocli-linux -o platform-linux
-curl -L https://github.com/openhie/package-starter-kit/releases/download/$cliVersion/gocli-macos -o platform-macos
-curl -L https://github.com/openhie/package-starter-kit/releases/download/$cliVersion/gocli.exe -o platform.exe
-chmod +x ./platform-linux
-chmod +x ./platform-macos
-chmod +x ./platform.exe
+case ${option} in 
+   linux)
+        echo "Downloading linux binary version: ${cliVersion}"
+        curl -L https://github.com/openhie/package-starter-kit/releases/download/$cliVersion/gocli-linux -o platform-linux
+        chmod +x ./platform-linux
+        exit 1
+      ;; 
+   macos)
+        echo "Downloading macos binary version: ${cliVersion}"
+        curl -L https://github.com/openhie/package-starter-kit/releases/download/$cliVersion/gocli-macos -o platform-macos
+        chmod +x ./platform-macos
+        exit 1
+      ;; 
+   windows)
+        echo "Downloading windows binary version: ${cliVersion}"
+        curl -L https://github.com/openhie/package-starter-kit/releases/download/$cliVersion/gocli.exe -o platform.exe
+        chmod +x ./platform.exe
+        exit 1
+      ;;
+    all)
+        echo "Downloading all binaries, version: ${cliVersion}"
+        curl -L https://github.com/openhie/package-starter-kit/releases/download/$cliVersion/gocli-linux -o platform-linux
+        curl -L https://github.com/openhie/package-starter-kit/releases/download/$cliVersion/gocli-macos -o platform-macos
+        curl -L https://github.com/openhie/package-starter-kit/releases/download/$cliVersion/gocli.exe -o platform.exe
+        chmod +x ./platform-linux
+        chmod +x ./platform-macos
+        chmod +x ./platform.exe
+        exit 1
+      ;;
+    --help)
+        echo "Usage: get-cli.sh [linux|macos|windows|all] {cliVersion}"
+        exit 1
+esac
