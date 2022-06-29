241ccf047830ea7b4c7741cf390004e4e7e60ee7
Castello Govender <castello.govender@jembi.org>
2022-06-29 13:25:27 +0200

    Replaced echo for jsreport

--- dashboard-visualiser-jsreport/swarm.sh
+++ dashboard-visualiser-jsreport/swarm.sh
@@ -17,54 +17,54 @@ ROOT_PATH="${COMPOSE_FILE_PATH}/.."
 . "${ROOT_PATH}/utils/config-utils.sh"
 
 if [[ "$MODE" == "dev" ]]; then
-  echo "Running JS Reports package in DEV mode"
+  log info "Running JS Reports package in DEV mode"
   js_report_dev_compose_param="-c ${COMPOSE_FILE_PATH}/docker-compose.dev.yml"
 else
-  echo "Running JS Reports package in PROD mode"
+  log info "Running JS Reports package in PROD mode"
   js_report_dev_compose_param=""
 fi
 
 configure_nginx() {
   if [[ "${INSECURE}" == "true" ]]; then
     docker config create --label name=nginx "${TIMESTAMP}-http-jsreport-insecure.conf" "${COMPOSE_FILE_PATH}/config/http-jsreport-insecure.conf"
-    echo "Updating nginx service: adding jsreport config file..."
+    log info "Updating nginx service: adding jsreport config file..."
     if ! docker service update \
       --config-add source="${TIMESTAMP}-http-jsreport-insecure.conf",target=/etc/nginx/conf.d/http-jsreport-insecure.conf \
       instant_reverse-proxy-nginx >/dev/null; then
-      echo "Error updating nginx service"
+      log error "Error updating nginx service"
       exit 1
     fi
-    echo "Done updating nginx service"
+    log info "Done updating nginx service"
   else
     docker config create --label name=nginx "${TIMESTAMP}-http-jsreport-secure.conf" "${COMPOSE_FILE_PATH}/config/http-jsreport-secure.conf"
-    echo "Updating nginx service: adding jsreport config file..."
+    log info "Updating nginx service: adding jsreport config file..."
     if ! docker service update \
       --config-add source="${TIMESTAMP}-http-jsreport-secure.conf",target=/etc/nginx/conf.d/http-jsreport-secure.conf \
       instant_reverse-proxy-nginx >/dev/null; then
-      echo "Error updating nginx service"
+      log error "Error updating nginx service"
       exit 1
     fi
-    echo "Done updating nginx service"
+    log info "Done updating nginx service"
   fi
 }
 
 if [[ "${JS_REPORT_DEV_MOUNT}" == "true" ]] && [[ "${ACTION}" == "init" ]]; then
   if [[ -z "${JS_REPORT_PACKAGE_PATH}" ]]; then
-    echo "ERROR: JS_REPORT_PACKAGE_PATH environment variable not specified. Please specify JS_REPORT_PACKAGE_PATH as stated in the README."
+    log error "ERROR: JS_REPORT_PACKAGE_PATH environment variable not specified. Please specify JS_REPORT_PACKAGE_PATH as stated in the README."
     exit 1
   fi
-  echo "MAKE SURE YOU HAVE RUN 'set-permissions.sh' SCRIPT BEFORE AND AFTER RUNNING JS REPORT"
+  log warning "MAKE SURE YOU HAVE RUN 'set-permissions.sh' SCRIPT BEFORE AND AFTER RUNNING JS REPORT"
 
-  echo "Attaching dev mount..."
+  log info "Attaching dev mount..."
   js_report_dev_mount_compose_param="-c ${COMPOSE_FILE_PATH}/docker-compose.dev-mnt.yml"
 fi
 
 main() {
   if [[ "${ACTION}" == "init" ]] || [[ "${ACTION}" == "up" ]]; then
-    docker stack deploy -c "$COMPOSE_FILE_PATH"/docker-compose.yml $js_report_dev_compose_param $js_report_dev_mount_compose_param instant
+    docker stack deploy -c "$COMPOSE_FILE_PATH"/docker-compose.yml ""$js_report_dev_compose_para"m"" "$js_report_dev_mount_compose_pa"ram" instant
 
     if [[ "${JS_REPORT_DEV_MOUNT}" != "true" ]]; then
-      echo "Verifying JS Reports service status"
+      log info "Verifying JS Reports service status"
       config::await_service_running "dashboard-visualiser-jsreport" "$COMPOSE_FILE_PATH"/docker-compose.await-helper.yml "$JS_REPORT_INSTANCES"
 
       config::set_config_digests "$COMPOSE_FILE_PATH"/importer/docker-compose.config.yml
@@ -82,7 +82,7 @@ main() {
   elif [[ "${ACTION}" == "destroy" ]]; then
     docker service rm instant_dashboard-visualiser-jsreport instant_jsreport-config-importer instant_await-helper &>/dev/null
   else
-    echo "Valid options are: init, up, down, or destroy"
+    log error "Valid options are: init, up, down, or destroy"
   fi
 }
 
