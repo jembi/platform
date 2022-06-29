fba29250c8e6426ba11bfef867795463febbed11
Castello Govender <castello.govender@jembi.org>
2022-06-29 13:29:08 +0200

    Replaced echo for hapi fhir helper

--- fhir-datastore-helper-hapi-fhir/swarm.sh
+++ fhir-datastore-helper-hapi-fhir/swarm.sh
@@ -4,30 +4,30 @@ hapiFhirReplicas=${HAPI_FHIR_INSTANCES:-1}
 fhirIgUrl=${FHIR_IG_URL}
 
 composeFilePath=$(
-  cd "$(dirname "${BASH_SOURCE[0]}")"
+  cd "$(dirname "${BASH_SOURCE[0]}")" || exit
   pwd -P
 )
 
 if [ -z "$fhirIgUrl" ]; then
-  echo "FHIR IG url should be specified using the env variable 'FHIR_IG_URL'"
+  log error "FHIR IG url should be specified using the env variable 'FHIR_IG_URL'"
 else
   if [ "$1" == "init" ]; then
     docker stack deploy -c "$composeFilePath"/docker-compose.yml instant
 
-    echo "Sleep 60 seconds to allow the FHIR IG import processs to finish"
+    log info "Sleep 60 seconds to allow the FHIR IG import processs to finish"
     sleep 60
 
     docker service scale instant_hapi-fhir=0
 
-    echo "Sleep 10 seconds to give HAPI-FHIR time to scale down"
+    log info "Sleep 10 seconds to give HAPI-FHIR time to scale down"
     sleep 10
 
-    docker service scale instant_hapi-fhir=$hapiFhirReplicas
+    docker service scale instant_hapi-fhir="$hapiFhirReplicas"
     docker service rm instant_hapi-fhir-config-importer
 
     # Sleep to allow the config importer to be removed. This is to enable the removal of the instant volume
     sleep 120
   else
-    echo "Valid option is init"
+    log error "Valid option is init"
   fi
 fi
