df52375b7db0a4131cde0e6c9338201b95e86a2e
Castello Govender <castello.govender@jembi.org>
2022-06-29 13:18:47 +0200

    Replaced echo for kafka

--- message-bus-kafka/swarm.sh
+++ message-bus-kafka/swarm.sh
@@ -3,30 +3,30 @@
 statefulNodes=${STATEFUL_NODES:-"cluster"}
 
 composeFilePath=$(
-  cd "$(dirname "${BASH_SOURCE[0]}")"
+  cd "$(dirname "${BASH_SOURCE[0]}")" || exit
   pwd -P
 )
 
 if [[ $statefulNodes == "cluster" ]]; then
-  printf "\nRunning Message Bus Kafka package in Cluster node mode\n"
+  log info "Running Message Bus Kafka package in Cluster node mode"
   kafkaClusterComposeParam="-c ${composeFilePath}/docker-compose.cluster.yml"
 else
-  printf "\nRunning Message Bus Kafka package in Single node mode\n"
+  log info "Running Message Bus Kafka package in Single node mode"
   kafkaClusterComposeParam=""
 fi
 
 if [[ $2 == "dev" ]]; then
-  printf "\nRunning Message Bus Kafka package in DEV mode\n"
+  log info "Running Message Bus Kafka package in DEV mode"
   kafkaDevComposeParam="-c ${composeFilePath}/docker-compose.dev.yml"
 else
-  printf "\nRunning Message Bus Kafka package in PROD mode\n"
+  log info "Running Message Bus Kafka package in PROD mode"
   kafkaDevComposeParam=""
 fi
 
 if [[ $1 == "init" ]]; then
-  docker stack deploy -c "$composeFilePath"/docker-compose.yml $kafkaClusterComposeParam $kafkaDevComposeParam instant
+  docker stack deploy -c "$composeFilePath"/docker-compose.yml "$kafkaClusterComposeParam" "$kafkaDevComposeParam" instant
 elif [[ $1 == "up" ]]; then
-  docker stack deploy -c "$composeFilePath"/docker-compose.yml $kafkaClusterComposeParam $kafkaDevComposeParam instant
+  docker stack deploy -c "$composeFilePath"/docker-compose.yml "$kafkaClusterComposeParam" "$kafkaDevComposeParam" instant
 elif [[ $1 == "down" ]]; then
   docker service scale instant_zookeeper-1=0 instant_kafdrop=0
   # You cannot scale a global service so we have to remove it
@@ -38,7 +38,7 @@ elif [[ $1 == "down" ]]; then
 elif [[ $1 == "destroy" ]]; then
   docker service rm instant_zookeeper-1 instant_kafka instant_kafdrop
 
-  echo "Sleep 20 Seconds to allow services to shut down before deleting volumes"
+  log info "Sleep 20 Seconds to allow services to shut down before deleting volumes"
   sleep 20
 
   docker volume rm instant_kafka-volume
@@ -47,8 +47,8 @@ elif [[ $1 == "destroy" ]]; then
   if [[ $statefulNodes == "cluster" ]]; then
     docker service rm instant_zookeeper-2
     docker service rm instant_zookeeper-3
-    echo "Volumes are only deleted on the host on which the command is run. Kafka volumes on other nodes are not deleted"
+    log notice "Volumes are only deleted on the host on which the command is run. Kafka volumes on other nodes are not deleted"
   fi
 else
-  echo "Valid options are: init, up, down, or destroy"
+  log error "Valid options are: init, up, down, or destroy"
 fi
