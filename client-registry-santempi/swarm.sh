1eaae174cc0584f34fce9adacace903fc3d3334a
michaelloosen <thorne199@gmail.com>
2022-06-30 16:45:02 +0200

    Update swarm.sh for Sante to use latest platform standards and add dependency on nginx To keep in sync with other packages

--- client-registry-santempi/swarm.sh
+++ client-registry-santempi/swarm.sh
@@ -1,70 +1,26 @@
 #!/bin/bash
 
-statefulNodes=${STATEFUL_NODES:-"cluster"}
-
-composeFilePath=$(
-  cd "$(dirname "${BASH_SOURCE[0]}")"
-  pwd -P
-)
+readonly ACTION=$1
+readonly MODE=$2
 
 TIMESTAMP="$(date "+%Y%m%d%H%M%S")"
 readonly TIMESTAMP
 
-if [ $statefulNodes == "cluster" ]; then
-  printf "\nRunning Client Registry SanteMPI package in Cluster node mode\n"
-  postgresClusterComposeParam="-c ${composeFilePath}/docker-compose-postgres.cluster.yml"
-else
-  printf "\nRunning Client Registry SanteMPI package in Single node mode\n"
-  postgresClusterComposeParam=""
-fi
+STATEFUL_NODES=${STATEFUL_NODES:-"cluster"}
 
-if [ "$2" == "dev" ]; then
-  printf "\nRunning Client Registry SanteMPI package in DEV mode\n"
-  postgresDevComposeParam="-c ${composeFilePath}/docker-compose-postgres.dev.yml"
-  santeMPIDevComposeParam="-c ${composeFilePath}/docker-compose.dev.yml"
-else
-  printf "\nRunning Client Registry SanteMPI package in PROD mode\n"
-  postgresDevComposeParam=""
-  santeMPIDevComposeParam=""
-fi
+COMPOSE_FILE_PATH=$(
+  cd "$(dirname "${BASH_SOURCE[0]}")" || exit
+  pwd -P
+)
+readonly COMPOSE_FILE_PATH
 
-if [ "$1" == "init" ]; then
-  docker stack deploy -c "$composeFilePath"/docker-compose-postgres.yml $postgresClusterComposeParam $postgresDevComposeParam instant
-
-  echo "Sleep 30 seconds to give Postgres time to start up before Sante MPI"
-  sleep 30
-
-  docker stack deploy -c "$composeFilePath"/docker-compose.yml $santeMPIDevComposeParam instant
-  if [[ "$2" != "dev" ]]; then
-    configure_nginx "$@"
-  fi
-elif [ "$1" == "up" ]; then
-  docker stack deploy -c "$composeFilePath"/docker-compose-postgres.yml $postgresClusterComposeParam $postgresDevComposeParam instant
-
-  echo "Sleep 20 seconds to give Postgres time to start up before Sante MPI"
-  sleep 20
-
-  docker stack deploy -c "$composeFilePath"/docker-compose.yml $santeMPIDevComposeParam instant
-elif [ "$1" == "down" ]; then
-  docker service scale instant_santedb-mpi=0 instant_santempi-psql-1=0 instant_santempi-psql-2=0 instant_santempi-psql-3=0
-elif [ "$1" == "destroy" ]; then
-  docker service rm instant_santedb-www instant_santedb-mpi instant_santempi-psql-1 instant_santempi-psql-2 instant_santempi-psql-3
-
-  echo "Sleep 10 Seconds to allow services to shut down before deleting volumes"
-  sleep 10
-
-  docker volume rm instant_santempi-psql-1-data instant_santempi-psql-2-data instant_santempi-psql-3-data
-
-  if [ $statefulNodes == "cluster" ]; then
-    echo "Volumes are only deleted on the host on which the command is run. Postgres volumes on other nodes are not deleted"
-  fi
-else
-  echo "Valid options are: init, up, down, or destroy"
-fi
+# Import libraries
+ROOT_PATH="${COMPOSE_FILE_PATH}/.."
+. "${ROOT_PATH}/utils/config-utils.sh"
 
 configure_nginx() {
   if [[ "${INSECURE}" == "true" ]]; then
-    docker config create --label name=nginx "${TIMESTAMP}-http-client-registry-santempi-insecure.conf" "${composeFilePath}/config/http-client-registry-santempi-insecure.conf"
+    docker config create --label name=nginx "${TIMESTAMP}-http-client-registry-santempi-insecure.conf" "${COMPOSE_FILE_PATH}/config/http-client-registry-santempi-insecure.conf"
     echo "Updating nginx service: adding client-registry-santempi config file..."
     if ! docker service update \
       --config-add source="${TIMESTAMP}-http-client-registry-santempi-insecure.conf",target=/etc/nginx/conf.d/http-client-registry-santempi-insecure.conf \
@@ -74,7 +30,7 @@ configure_nginx() {
     fi
     echo "Done updating nginx service"
   else
-    docker config create --label name=nginx "${TIMESTAMP}-http-client-registry-santempi-secure.conf" "${composeFilePath}/config/http-client-registry-santempi-secure.conf"
+    docker config create --label name=nginx "${TIMESTAMP}-http-client-registry-santempi-secure.conf" "${COMPOSE_FILE_PATH}/config/http-client-registry-santempi-secure.conf"
     echo "Updating nginx service: adding client-registry-santempi config file..."
     if ! docker service update \
       --config-add source="${TIMESTAMP}-http-client-registry-santempi-secure.conf",target=/etc/nginx/conf.d/http-client-registry-santempi-secure.conf \
@@ -85,3 +41,62 @@ configure_nginx() {
     echo "Done updating nginx service"
   fi
 }
+
+main() {
+if [ $STATEFUL_NODES == "cluster" ]; then
+  printf "\nRunning Client Registry SanteMPI package in Cluster node mode\n"
+  POSTGRES_CLUSTER_COMPOSE_PARAM="-c ${COMPOSE_FILE_PATH}/docker-compose-postgres.cluster.yml"
+else
+  printf "\nRunning Client Registry SanteMPI package in Single node mode\n"
+  POSTGRES_CLUSTER_COMPOSE_PARAM=""
+fi
+
+if [ "$MODE" == "dev" ]; then
+  printf "\nRunning Client Registry SanteMPI package in DEV mode\n"
+  POSTGRES_DEV_COMPOSE_PARAM="-c ${COMPOSE_FILE_PATH}/docker-compose-postgres.dev.yml"
+  SANTE_MPI_DEV_COMPOSE_PARAM="-c ${COMPOSE_FILE_PATH}/docker-compose.dev.yml"
+else
+  printf "\nRunning Client Registry SanteMPI package in PROD mode\n"
+  POSTGRES_DEV_COMPOSE_PARAM=""
+  SANTE_MPI_DEV_COMPOSE_PARAM=""
+fi
+
+if [ "$ACTION" == "init" ]; then
+  docker stack deploy -c "$COMPOSE_FILE_PATH"/docker-compose-postgres.yml $POSTGRES_CLUSTER_COMPOSE_PARAM $POSTGRES_DEV_COMPOSE_PARAM instant
+
+  echo "Sleep 30 seconds to give Postgres time to start up before Sante MPI"
+  sleep 30
+  #TODO: Replace this sleep with await helper
+
+  docker stack deploy -c "$COMPOSE_FILE_PATH"/docker-compose.yml $SANTE_MPI_DEV_COMPOSE_PARAM instant
+  if [[ "$MODE" != "dev" ]]; then
+    configure_nginx "$@"
+  fi
+elif [ "$ACTION" == "up" ]; then
+  docker stack deploy -c "$COMPOSE_FILE_PATH"/docker-compose-postgres.yml $POSTGRES_CLUSTER_COMPOSE_PARAM $POSTGRES_DEV_COMPOSE_PARAM instant
+
+  echo "Sleep 20 seconds to give Postgres time to start up before Sante MPI"
+  sleep 20
+  #TODO: Replace this sleep with await helper
+
+  docker stack deploy -c "$COMPOSE_FILE_PATH"/docker-compose.yml $SANTE_MPI_DEV_COMPOSE_PARAM instant
+elif [ "$1" == "down" ]; then
+  docker service scale instant_santedb-mpi=0 instant_santempi-psql-1=0 instant_santempi-psql-2=0 instant_santempi-psql-3=0
+elif [ "$1" == "destroy" ]; then
+  docker service rm instant_santedb-www instant_santedb-mpi instant_santempi-psql-1 instant_santempi-psql-2 instant_santempi-psql-3
+
+  echo "Sleep 10 Seconds to allow services to shut down before deleting volumes"
+  sleep 10
+  #TODO: Replace this sleep with await helper
+
+  docker volume rm instant_santempi-psql-1-data instant_santempi-psql-2-data instant_santempi-psql-3-data
+
+  if [ $STATEFUL_NODES == "cluster" ]; then
+    echo "Volumes are only deleted on the host on which the command is run. Postgres volumes on other nodes are not deleted"
+  fi
+else
+  echo "Valid options are: init, up, down, or destroy"
+fi
+}
+
+main "$@"
