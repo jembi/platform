8f898e9841842f5c027f8c1e0b5ddb2d907532bc
mark-labuschagne <marklabuschagne7@gmail.com>
2022-07-08 09:36:24 +0200

    Reintroduced accidentally deleted files

---
+++ dashboard-visualiser-jsreport/scripts/elasticSearchConnTestScript/content.js
@@ -0,0 +1,18 @@
+// Use the "beforeRender" or "afterRender" hook
+// to manipulate and control the report generation
+const axios = require('axios')
+
+async function beforeRender (req, res) {
+    const resData = await axios({
+        method: 'get',
+        url: `http://analytics-datastore-elastic-search:9200/_cat/health/`,
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Basic ${new Buffer(
+            `elastic:dev_password_only`
+            ).toString('base64')}`
+        }
+    })
+
+    req.data = Object.assign({}, {healthcheck: resData.data})
+}
