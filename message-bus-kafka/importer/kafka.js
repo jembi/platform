024d3a8398b90ef592989f8066563214d748c924
bradsawadye <bradsawadye@gmail.com>
2022-07-19 12:35:43 +0200

    Configure kafka
    This creates the kafka topics
    
    PLAT-346

---
+++ message-bus-kafka/importer/kafka.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const http = require('http')
+
+const KAFDROP_HOST = process.env.KAFDROP_HOST || 'kafdrop'
+const KAFDROP_PORT = process.env.KAFDROP_PORT || '9013'
+const KAFKA_PARTITIONS = process.env.KAFKA_PARTITIONS || 3
+const KAFKA_TOPICS= process.env.KAFKA_TOPICS
+
+const createKafkaTopic = topic => {
+  const options = {
+    protocol: 'http:',
+    hostname: KAFDROP_HOST,
+    path: '/topic',
+    port: KAFDROP_PORT,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }
+
+  const req = http.request(options, res => {
+    if (res.statusCode != 200) {
+      throw Error(`Failed to create topic - ${topic}`)
+    }
+    console.log(`Created topic - ${topic}`)
+  })
+  req.write(`name=${topic}&partitionsNumber=${KAFKA_PARTITIONS}&replicationFactor=1`)
+  req.end()
+}
+
+console.log('Creating kafka topics......................');
+
+(() => {
+  if (KAFKA_TOPICS) {
+   KAFKA_TOPICS.split(',').forEach(topic => createKafkaTopic(topic))
+  } else {
+    console.log('Topics not created: KAFKA_TOPICS variable invalid')
+  }
+})();
