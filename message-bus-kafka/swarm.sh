87e585382a2b9b0462a1020747940b8785b4a6a9
bradsawadye <bradsawadye@gmail.com>
2022-07-21 14:11:19 +0200

    Wait for kafka to start up before configuring
    The configuration can only be done once the service is up. THis ensures that the services for configuring dont fail

--- message-bus-kafka/swarm.sh
+++ message-bus-kafka/swarm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-statefulNodes=${STATEFUL_NODES:-"cluster"}
+STATEFUL_NODES=${STATEFUL_NODES:-"cluster"}
 
 COMPOSE_FILE_PATH=$(
   cd "$(dirname "${BASH_SOURCE[0]}")" || exit
@@ -13,7 +13,7 @@ ROOT_PATH="${COMPOSE_FILE_PATH}/.."
 . "${ROOT_PATH}/utils/docker-utils.sh"
 . "${ROOT_PATH}/utils/log.sh"
 
-if [[ $statefulNodes == "cluster" ]]; then
+if [[ $STATEFUL_NODES == "cluster" ]]; then
   log info "Running Message Bus Kafka package in Cluster node mode"
   kafkaClusterComposeParam="-c ${COMPOSE_FILE_PATH}/docker-compose.cluster.yml"
 else
@@ -29,10 +29,39 @@ else
   kafkaDevComposeParam=""
 fi
 
+verify_kafka() {
+  local start_time
+  start_time=$(date +%s)
+  until [[ $(docker service ls -f name=instant_kafka --format "{{.Replicas}}") == *"${KAFKA_INSTANCES}/${KAFKA_INSTANCES}"* ]]; do
+    config::timeout_check "${start_time}" "kafka to start"
+    sleep 1
+  done
+
+  local await_helper_state
+  await_helper_state=$(docker service ps instant_await-helper --format "{{.CurrentState}}")
+  until [[ "${await_helper_state}" == *"Complete"* ]]; do
+    config::timeout_check "${start_time}" "kafka heartbeat check"
+    sleep 1
+
+    await_helper_state=$(docker service ps instant_await-helper --format "{{.CurrentState}}")
+    if [[ "${await_helper_state}" == *"Failed"* ]] || [[ "${await_helper_state}" == *"Rejected"* ]]; then
+      log error "Fatal: Received error when trying to verify state of kafka. Error:
+       $(docker service ps instant_await-helper --no-trunc --format '{{.Error}}')"
+      exit 1
+    fi
+  done
+
+  try "docker service rm instant_await-helper" "Failed to remove await helper"
+}
+
 if [[ $1 == "init" ]] || [[ $1 == "up" ]]; then
   config::set_config_digests "${COMPOSE_FILE_PATH}"/importer/docker-compose.config.yml
 
   try "docker stack deploy -c ${COMPOSE_FILE_PATH}/docker-compose.yml $kafkaClusterComposeParam $kafkaDevComposeParam instant" "Failed to deploy Message Bus Kafka"
+  try "docker stack deploy -c ${COMPOSE_FILE_PATH}/docker-compose.await-helper.yml instant" "Failed to deploy await-helper"
+
+  verify_kafka
+
   try "docker stack deploy -c ${COMPOSE_FILE_PATH}/importer/docker-compose.config.yml instant" "Failed to deploy Message Bus Kafka"
 
   config::remove_stale_service_configs "${COMPOSE_FILE_PATH}"/importer/docker-compose.config.yml "ethiopia"
@@ -41,7 +70,7 @@ elif [[ $1 == "down" ]]; then
   try "docker service scale instant_zookeeper-1=0 instant_kafdrop=0" "Failed to scale down zookeeper and kafdrop"
   # You cannot scale a global service so we have to remove it
   try "docker service rm instant_kafka" "Failed to remove kafka"
-  if [[ $statefulNodes == "cluster" ]]; then
+  if [[ $STATEFUL_NODES == "cluster" ]]; then
     try "docker service scale instant_zookeeper-2=0" "Failed to scale down zookeeper cluster"
     try "docker service scale instant_zookeeper-3=0" "Failed to scale down zookeeper cluster"
   fi
@@ -57,7 +86,7 @@ elif [[ $1 == "destroy" ]]; then
   try "docker volume rm instant_kafka-volume" "Failed to remove kafka volume"
   try "docker volume rm instant_zookeeper-1-volume" "Failed to remove zookeeper volume"
 
-  if [[ $statefulNodes == "cluster" ]]; then
+  if [[ $STATEFUL_NODES == "cluster" ]]; then
     try "docker service rm instant_zookeeper-2" "Failed to remove zookeeper cluster volumes"
     try "docker service rm instant_zookeeper-3" "Failed to remove zookeeper cluster volumes"
     log notice "Volumes are only deleted on the host on which the command is run. Kafka volumes on other nodes are not deleted"
