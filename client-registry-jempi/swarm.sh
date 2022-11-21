3db96998fe381f1b46e6efb5a17834c86849cd39
mark-labuschagne <marklabuschagne7@gmail.com>
2022-11-21 13:43:11 +0200

    added dgraph to jempi package
    PLAT-617

--- client-registry-jempi/swarm.sh
+++ client-registry-jempi/swarm.sh
@@ -17,12 +17,22 @@ readonly ROOT_PATH
 . "${ROOT_PATH}/utils/log.sh"
 
 main() {
-  if [[ "${MODE}" == "dev" ]]; then
+  if [[ "$MODE" == "dev" ]]; then
     log info "Running Client Registry JeMPI package in DEV mode"
     kafdrop_dev_compose_param="-c ${COMPOSE_FILE_PATH}/docker-compose.kafdrop-dev.yml"
+    dgraph_dev_compose_param="-c ${COMPOSE_FILE_PATH}/docker-compose.dgraph-dev.yml"
   else
     log info "Running Client Registry JeMPI package in PROD mode"
     kafdrop_dev_compose_param=""
+    dgraph_dev_compose_param=""
+  fi
+
+  if [[ "$STATEFUL_NODES" == "cluster" ]]; then
+    log info "Running in clustered mode"
+    dgraph_cluster_compose_param="-c ${COMPOSE_FILE_PATH}/docker-compose.dgraph-cluster.yml"
+  else
+    log info "Running in single-node mode"
+    dgraph_cluster_compose_param=""
   fi
 
   if [[ "${ACTION}" == "init" ]] || [[ "${ACTION}" == "up" ]]; then
@@ -57,27 +67,73 @@ main() {
 
     config::remove_stale_service_configs "${COMPOSE_FILE_PATH}"/importer/docker-compose.config.yml "jempi-kafka"
 
-    docker::deploy_sanity "kafka-01" "kafka-02" "kafka-03" "jempi-kafdrop"
+    try "docker stack deploy -c ${COMPOSE_FILE_PATH}/docker-compose.dgraph.yml $dgraph_dev_compose_param $dgraph_cluster_compose_param instant" "Failed to deploy Client Registry - JeMPI"
+
+    docker::await_container_startup zero-01
+    docker::await_container_status zero-01 Running
+    config::await_network_join instant_zero-01
+
+    docker::await_container_startup alpha-01
+    docker::await_container_startup alpha-02
+    docker::await_container_startup alpha-03
+
+    docker::await_container_status alpha-01 Running
+    docker::await_container_status alpha-02 Running
+    docker::await_container_status alpha-03 Running
+
+    config::await_network_join instant_alpha-01
+    config::await_network_join instant_alpha-02
+    config::await_network_join instant_alpha-03
+
+    docker::await_container_startup ratel
+    docker::await_container_status ratel Running
+    config::await_network_join instant_ratel
+
+    docker::deploy_sanity "kafka-01" "kafka-02" "kafka-03" "jempi-kafdrop" "zero-01" "alpha-01" "alpha-02" "alpha-03" "ratel"
   elif [[ "${ACTION}" == "down" ]]; then
+    log info "Scaling down client-registry-jempi"
+
     try "docker service scale instant_kafka-01=0" "Failed to scale down kafka-01"
     try "docker service scale instant_kafka-02=0" "Failed to scale down kafka-02"
     try "docker service scale instant_kafka-03=0" "Failed to scale down kafka-03"
 
     try "docker service scale instant_jempi-kafdrop=0" "Failed to scale down jempi-kafdrop"
+
+    try "docker service scale instant_zero-01=0" "Failed to scale down zero-01"
+
+    try "docker service scale instant_alpha-01=0" "Failed to scale down alpha-01"
+    try "docker service scale instant_alpha-02=0" "Failed to scale down alpha-02"
+    try "docker service scale instant_alpha-03=0" "Failed to scale down alpha-03"
+
+    try "docker service scale instant_ratel=0" "Failed to scale down ratel"
   elif [[ "${ACTION}" == "destroy" ]]; then
     log warn "Volumes are only deleted on the host on which the command is run. Volumes on other nodes are not deleted"
 
     docker::service_destroy kafka-01
     docker::service_destroy kafka-02
     docker::service_destroy kafka-03
-    
+
     docker::service_destroy jempi-kafdrop
-    
+
     docker::service_destroy jempi-kafka-config-importer
+
+    docker::service_destroy zero-01
+
+    docker::service_destroy alpha-01
+    docker::service_destroy alpha-02
+    docker::service_destroy alpha-03
+
+    docker::service_destroy ratel
 
     docker::try_remove_volume kafka-01-data
     docker::try_remove_volume kafka-02-data
     docker::try_remove_volume kafka-03-data
+
+    docker::try_remove_volume zero-01-data
+
+    docker::try_remove_volume alpha-01-data
+    docker::try_remove_volume alpha-02-data
+    docker::try_remove_volume alpha-03-data
 
     docker::prune_configs "jempi-kafka"
   else
