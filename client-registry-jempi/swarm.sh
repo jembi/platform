868d5499c4ced56774705c26826f8e1b32008c03
mark-labuschagne <marklabuschagne7@gmail.com>
2022-11-21 11:26:31 +0200

    cleaned files for kafka deployment we are going to stick to using kafdrop to import topics, this commit just tidies everything up and reverts the changes of trying to import topics in a different way PLAT-616

--- client-registry-jempi/swarm.sh
+++ client-registry-jempi/swarm.sh
@@ -26,8 +26,6 @@ main() {
   fi
 
   if [[ "${ACTION}" == "init" ]] || [[ "${ACTION}" == "up" ]]; then
-    config::set_config_digests "${COMPOSE_FILE_PATH}"/docker-compose.kafka.yml
-
     try "docker stack deploy -c ${COMPOSE_FILE_PATH}/docker-compose.kafka.yml instant" "Failed to deploy Client Registry - JeMPI"
 
     docker::await_container_startup kafka-01
@@ -38,43 +36,51 @@ main() {
     docker::await_container_status kafka-02 Running
     docker::await_container_status kafka-03 Running
 
-    config::await_network_join "instant_kafka-01"
-    config::await_network_join "instant_kafka-02"
-    config::await_network_join "instant_kafka-03"
+    config::await_network_join instant_kafka-01
+    config::await_network_join instant_kafka-02
+    config::await_network_join instant_kafka-03
 
     try "docker stack deploy -c ${COMPOSE_FILE_PATH}/docker-compose.kafdrop.yml $kafdrop_dev_compose_param instant" "Failed to deploy jempi-Kafdrop"
 
     docker::await_container_startup jempi-kafdrop
     docker::await_container_status jempi-kafdrop Running
-    config::await_network_join "instant_jempi-kafdrop"
+    config::await_network_join instant_jempi-kafdrop
 
-    config::remove_stale_service_configs "${COMPOSE_FILE_PATH}"/docker-compose.kafka.yml "jempi-kafka"
+    config::set_config_digests "${COMPOSE_FILE_PATH}"/importer/docker-compose.config.yml
 
-    # config::set_config_digests "${COMPOSE_FILE_PATH}"/importer/docker-compose.config.yml
+    try "docker stack deploy -c ${COMPOSE_FILE_PATH}/importer/docker-compose.config.yml instant" "Failed to deploy jempi-kafka-config-importer"
 
-    # try "docker stack deploy -c ${COMPOSE_FILE_PATH}/importer/docker-compose.config.yml instant" "Failed to deploy jempi-kafka-config-importer"
+    log info "Waiting to give JeMPI Kafka config importer time to run before cleaning up service"
 
-    # log info "Waiting to give JeMPI Kafka config importer time to run before cleaning up service"
+    config::remove_config_importer jempi-kafka-config-importer
+    config::await_service_removed instant_jempi-kafka-config-importer
 
-    # config::remove_config_importer jempi-kafka-config-importer
-
-    # config::await_service_removed instant_jempi-kafka-config-importer
-
-    # config::remove_stale_service_configs "${COMPOSE_FILE_PATH}"/importer/docker-compose.config.yml "jempi-kafka"
+    config::remove_stale_service_configs "${COMPOSE_FILE_PATH}"/importer/docker-compose.config.yml "jempi-kafka"
 
     docker::deploy_sanity "kafka-01" "kafka-02" "kafka-03" "jempi-kafdrop"
-  # elif [[ "${ACTION}" == "down" ]]; then
-  #   try "docker service scale instant_dashboard-visualiser-superset=0" "Failed to scale down dashboard-visualiser-superset"
-  # elif [[ "${ACTION}" == "destroy" ]]; then
-  #   docker::service_destroy dashboard-visualiser-superset
-  #   docker::service_destroy jempi-kafka-config-importer
+  elif [[ "${ACTION}" == "down" ]]; then
+    try "docker service scale instant_kafka-01=0" "Failed to scale down kafka-01"
+    try "docker service scale instant_kafka-02=0" "Failed to scale down kafka-02"
+    try "docker service scale instant_kafka-03=0" "Failed to scale down kafka-03"
 
-  #   # Removing Superset volumes
-  #   docker::try_remove_volume superset
-  #   docker::try_remove_volume superset-frontend
-  #   docker::try_remove_volume superset_home
+    try "docker service scale instant_jempi-kafdrop=0" "Failed to scale down jempi-kafka-03"
+  elif [[ "${ACTION}" == "destroy" ]]; then
+    log warn "Volumes are only deleted on the host on which the command is run. Elastic Search volumes on other nodes are not deleted"
 
-  #   docker::prune_configs "superset"
+    docker::service_destroy kafka-01
+    docker::service_destroy kafka-02
+    docker::service_destroy kafka-03
+    
+    docker::service_destroy jempi-kafdrop
+    
+    docker::service_destroy jempi-kafka-config-importer
+
+    # Removing Superset volumes
+    docker::try_remove_volume kafka-01-data
+    docker::try_remove_volume kafka-02-data
+    docker::try_remove_volume kafka-03-data
+
+    docker::prune_configs "jempi-kafka"
   else
     log error "Valid options are: init, up, down, or destroy"
   fi
