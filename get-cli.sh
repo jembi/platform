ac2df2c759bc83204e85f8c0fe6f1d996d199056
michaelloosen <thorne199@gmail.com>
2023-02-06 16:27:03 +0200

    Fix get-cli pulling incorrect name of CLI To fix CI build PLAT-665

--- get-cli.sh
+++ get-cli.sh
@@ -6,27 +6,27 @@ cli_version=${2:-latest}
 case ${option} in
 linux)
   echo "Downloading linux binary version: ${cli_version}"
-  curl -L https://github.com/openhie/package-starter-kit/releases/download/"$cli_version"/gocli-linux -o instant-linux
+  curl -L https://github.com/openhie/package-starter-kit/releases/download/"$cli_version"/instant-linux -o instant-linux
   chmod +x ./instant-linux
   exit 0
   ;;
 macos)
   echo "Downloading macos binary version: ${cli_version}"
-  curl -L https://github.com/openhie/package-starter-kit/releases/download/"$cli_version"/gocli-macos -o instant-macos
+  curl -L https://github.com/openhie/package-starter-kit/releases/download/"$cli_version"/instant-macos -o instant-macos
   chmod +x ./instant-macos
   exit 0
   ;;
 windows)
   echo "Downloading windows binary version: ${cli_version}"
-  curl -L https://github.com/openhie/package-starter-kit/releases/download/"$cli_version"/gocli.exe -o instant.exe
+  curl -L https://github.com/openhie/package-starter-kit/releases/download/"$cli_version"/instant.exe -o instant.exe
   chmod +x ./instant.exe
   exit 0
   ;;
 all)
   echo "Downloading all binaries, version: ${cli_version}"
-  curl -L https://github.com/openhie/package-starter-kit/releases/download/"$cli_version"/gocli-linux -o instant-linux
-  curl -L https://github.com/openhie/package-starter-kit/releases/download/"$cli_version"/gocli-macos -o instant-macos
-  curl -L https://github.com/openhie/package-starter-kit/releases/download/"$cli_version"/gocli.exe -o instant.exe
+  curl -L https://github.com/openhie/package-starter-kit/releases/download/"$cli_version"/instant-linux -o instant-linux
+  curl -L https://github.com/openhie/package-starter-kit/releases/download/"$cli_version"/instant-macos -o instant-macos
+  curl -L https://github.com/openhie/package-starter-kit/releases/download/"$cli_version"/instant.exe -o instant.exe
   chmod +x ./instant-linux
   chmod +x ./instant-macos
   chmod +x ./instant.exe
