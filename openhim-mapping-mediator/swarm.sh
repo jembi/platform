b6ab399006f86f884278b49f6a36826a730b684d
Arran Standish <arranstandish@hotmail.com>
2023-03-15 12:41:30 +0200

    Update remaning packages to parameterise their stack name and connect to relevant networks

--- openhim-mapping-mediator/swarm.sh
+++ openhim-mapping-mediator/swarm.sh
@@ -5,6 +5,7 @@ declare MODE=""
 declare COMPOSE_FILE_PATH=""
 declare UTILS_PATH=""
 declare SERVICE_NAMES=()
+declare STACK="openhim-mapping-mediator"
 
 function init_vars() {
   ACTION=$1
@@ -17,13 +18,11 @@ function init_vars() {
 
   UTILS_PATH="${COMPOSE_FILE_PATH}/../utils"
 
-  SERVICE_NAMES=("openhim-mapping-mediator")
-
   readonly ACTION
   readonly MODE
   readonly COMPOSE_FILE_PATH
   readonly UTILS_PATH
-  readonly SERVICE_NAMES
+  readonly STACK
 }
 
 # shellcheck disable=SC1091
@@ -43,8 +42,7 @@ function initialize_package() {
   fi
 
   (
-    docker::deploy_service "${COMPOSE_FILE_PATH}" "docker-compose.yml" "$openhim_mapping_dev_compose_filename"
-    docker::deploy_sanity "${SERVICE_NAMES}"
+    docker::deploy_service $STACK "${COMPOSE_FILE_PATH}" "docker-compose.yml" "$openhim_mapping_dev_compose_filename"
   ) || {
     log error "Failed to deploy package"
     exit 1
@@ -52,7 +50,7 @@ function initialize_package() {
 }
 
 function destroy_package() {
-  docker::service_destroy "${SERVICE_NAMES}"
+  docker::stack_destroy $STACK
 }
 
 main() {
@@ -70,7 +68,7 @@ main() {
   elif [[ "${ACTION}" == "down" ]]; then
     log info "Scaling down package"
 
-    docker::scale_services_down "${SERVICE_NAMES}"
+    docker::scale_services $STACK 0
   elif [[ "${ACTION}" == "destroy" ]]; then
     log info "Destroying package"
     destroy_package
