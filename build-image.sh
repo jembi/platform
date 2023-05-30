183a13d72df0e34a4cc36c799658c18bdc1ea4be
michaelloosen <thorne199@gmail.com>
2023-05-30 13:12:22 +0200

    Update build-image.sh script to fix tests The cucumber tests require the image build by the CI to match the tag specified in the config.yaml file.

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-TAG_NAME=${1:-latest}
-docker build -t jembi/platform:$TAG_NAME .
+TAG_NAME=${1:-latest} #Default to latest if no parameter supplied and no version is specified in the config.yaml file
+CONFIG_TAG=$(docker run --rm -v "${PWD}":/workdir mikefarah/yq:4.24.5 '.image' "config.yaml")
+if [ "$CONFIG_TAG" != "null" ]; then
+    TagVersion=${CONFIG_TAG:15}
+    if [ -n "$TagVersion" ]; then
+        if [[ -z "$1" ]]; then
+            #Override the TAG_NAME with the one specified in config.yaml only if no parameter was supplied
+            TAG_NAME=$TagVersion
+        fi
+    fi
+fi
+docker build -t jembi/platform:"$TAG_NAME" .
