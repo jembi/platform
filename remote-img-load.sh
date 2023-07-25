b078a2ddbcb4ebfb1988b752e9b8e42a388668e9
bradsawadye <bradsawadye@gmail.com>
2023-07-25 14:43:31 +0200

    Fix the remote upload functionality
    This enables us to build the image and use it to test the changes in the pr

--- remote-img-load.sh
+++ remote-img-load.sh
@@ -2,24 +2,28 @@
 
 readonly HOST="$1"
 readonly USER=${2:-ubuntu}
+TAG_NAME=${3:-latest} #Default to latest if version is not supplied
+
 if [ -z "${HOST}" ]; then
   echo "Usage: ./remote-img-load.sh <host>"
   exit 1
 fi
 
-TAG_NAME=${1:-latest} #Default to latest if no parameter supplied and no version is specified in the config.yaml file
 CONFIG_TAG=$(docker run --rm -v "${PWD}":/workdir mikefarah/yq:4.24.5 '.image' "config.yaml")
 if [ "$CONFIG_TAG" != "null" ]; then
   TagVersion=${CONFIG_TAG:15}
   if [ -n "$TagVersion" ]; then
-    if [[ -z "$1" ]]; then
+    if [[ -z "$3" ]]; then
       #Override the TAG_NAME with the one specified in config.yaml only if no parameter was supplied
       TAG_NAME=$TagVersion
     fi
   fi
 fi
-
+ echo $TAG_NAME
+ echo $HOST
+ echo $USER
+ 
 ./build-image.sh "$TAG_NAME"
 echo "Transfering image to ${HOST} ..."
-docker save jembi/platform:latest | bzip2 | ssh "${USER}@${HOST}" docker load
+docker save jembi/platform:"$TAG_NAME" | bzip2 | ssh "${USER}@${HOST}" docker load
 echo "Image transfered"
