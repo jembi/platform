7688185456f2843108fe12ac004925a7414ac532
Arran Standish <arranstandish@hotmail.com>
2023-09-01 12:00:58 +0200

    Remove redundant network definition from pgpool & fix swarm.sh if check

--- fhir-datastore-hapi-fhir/swarm.sh
+++ fhir-datastore-hapi-fhir/swarm.sh
@@ -51,7 +51,7 @@ function initialize_package() {
   (
     docker::deploy_service "$STACK" "${COMPOSE_FILE_PATH}" "docker-compose-postgres.yml" "$postgres_cluster_compose_filename" "$postgres_dev_compose_filename"
 
-    if [ "${CLUSTERED_MODE}" == "true" && "${HF_PGPOOL_ENABLED}" == "true" ]; then
+    if [[ "${CLUSTERED_MODE}" == "true" && "${HF_PGPOOL_ENABLED}" == "true" ]]; then
       docker::deploy_service "$STACK" "${COMPOSE_FILE_PATH}" "docker-compose-pgpool.cluster.yml"
     fi
 
