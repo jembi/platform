965652de8ea07ed42e1942810068d03d79f02c66
bradsawadye <bradsawadye@gmail.com>
2024-02-23 13:34:38 +0200

    Fix the logic for determining tables to create
    tHe logic was broken because the Boolean function that was being used, returns true for all the strings ("false" and "true"). The env variable is always a string.

--- analytics-datastore-clickhouse/importer/config/clickhouseTables.js
+++ analytics-datastore-clickhouse/importer/config/clickhouseTables.js
@@ -1,31 +1,8 @@
-const CLUSTERED_MODE = process.env.CLUSTERED_MODE || "true";
+const CLUSTERED_MODE = process.env.CLUSTERED_MODE || "false";
 
 const queries =
-  Boolean(CLUSTERED_MODE) === true
+  CLUSTERED_MODE === "true"
     ? [
-        `CREATE TABLE patient_example(
-					id String,
-					version String NULL,
-					inserted_at DateTime DEFAULT now(),
-					last_updated Date NULL,
-					goldenId String,
-					patientGivenName String,
-					patientFamilyName String,
-				) 
-				ENGINE=MergeTree
-				ORDER BY tuple();`,
-        `CREATE TABLE observation_example(
-					id String,
-					version String NULL,
-					inserted_at DateTime DEFAULT now(),
-					last_updated Date NULL,
-					observationValue Double,
-					patientId String,
-				) 
-				ENGINE=MergeTree
-				ORDER BY tuple();`,
-      ]
-    : [
         `CREATE TABLE patient_example ON CLUSTER '{cluster}' (
 					id String,
 					version String NULL,
@@ -46,6 +23,29 @@ const queries =
 					patientId String,
 				) 
 				ENGINE = ReplicatedMergeTree('/clickhouse/tables/{cluster}/{shard}/{table}', '{replica}')
+				ORDER BY tuple();`,
+      ]
+	: [
+        `CREATE TABLE patient_example(
+					id String,
+					version String NULL,
+					inserted_at DateTime DEFAULT now(),
+					last_updated Date NULL,
+					goldenId String,
+					patientGivenName String,
+					patientFamilyName String,
+				) 
+				ENGINE=MergeTree
+				ORDER BY tuple();`,
+        `CREATE TABLE observation_example(
+					id String,
+					version String NULL,
+					inserted_at DateTime DEFAULT now(),
+					last_updated Date NULL,
+					observationValue Double,
+					patientId String,
+				) 
+				ENGINE=MergeTree
 				ORDER BY tuple();`,
       ];
 
