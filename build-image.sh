187dac276a4553347a26e8c683d23310830e3ff8
Arran Standish <arranstandish@hotmail.com>
2024-03-06 09:33:21 +0200

    Simplify image tag determination + remove yq dependency

--- build-image.sh
+++ build-image.sh
@@ -3,10 +3,12 @@ TAG_NAME=${1:-latest}
 
 # We did not specify a tag so try and use the tag in the config.yaml if present
 if [ -z "$1" ]; then
-    ImageTag=$(docker run --rm -v "${PWD}":/workdir mikefarah/yq:4.24.5 '.image' "config.yaml")
+    # we grep out 'image: jembi/platform:2.x' from which we cut on : and choose the last column
+    # this will always be the image tag or an empty string
+    ImageTag=$(grep 'image:' ${PWD}/config.yaml | cut -d : -f 3)
     # only overwrite TAG_NAME if we have a tag present, and it's not just the base image name
-    if [[ $ImageTag =~ ":" ]]; then
-        TAG_NAME=${ImageTag#*:}
+    if [ -n "$ImageTag" ]; then
+        TAG_NAME=${ImageTag}
     fi
 fi
 
