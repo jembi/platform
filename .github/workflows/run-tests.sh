435e1d9bd0c7a298437ba11bce0cdfe8e6f91f88
bradsawadye <bradsawadye@gmail.com>
2024-04-12 15:52:10 +0200

    Run one feature to test changes in the utils, steps and infrastructure folder
    You only need to run tests in one feature to ensure that the functionality used in all the features still works

--- .github/workflows/run-tests.sh
+++ .github/workflows/run-tests.sh
@@ -25,13 +25,8 @@ for package in "${CHANGED_FILES[@]}"; do
     fi
 done
 
-if [[ ${#changed_packages[@]} -eq 0 ]] || [[ "${!changed_packages[*]}" == *"utils"* ]]; then
-    DOCKER_HOST=ssh://ubuntu@$GITHUB_RUN_ID.jembi.cloud yarn test:"$NODE_MODE"
-elif [[ "${!changed_packages[*]}" == *"features/single-mode"* ]] && [[ $NODE_MODE == "single" ]]; then
-    DOCKER_HOST=ssh://ubuntu@$GITHUB_RUN_ID.jembi.cloud yarn test:single
-elif [[ "${!changed_packages[*]}" == *"features/cluster-mode"* ]] && [[ $NODE_MODE == "cluster" ]]; then
-    DOCKER_HOST=ssh://ubuntu@$GITHUB_RUN_ID.jembi.cloud yarn test:cluster
-elif [[ "${!changed_packages[*]}" == *"infrastructure"* ]] && [[ $openhimRan == "false" ]]; then
+if [[ ${#changed_packages[@]} -eq 0 ]] || [[ "${!changed_packages[*]}" == *"utils"* ]] || [[ "${!changed_packages[*]}" == *"features/steps"* ]] || [[ "${!changed_packages[*]}" == *"infrastructure"* ]] ; then
+    openhim_ran="true"
     DOCKER_HOST=ssh://ubuntu@$GITHUB_RUN_ID.jembi.cloud yarn test:"$NODE_MODE":openhim
 else
     for folder_name in "${!changed_packages[@]}"; do
@@ -70,4 +65,3 @@ fi
 
 # Run the basic funtional end to end tests for the CDR recipe
 DOCKER_HOST=ssh://ubuntu@$GITHUB_RUN_ID.jembi.cloud yarn test:"$NODE_MODE":recipe
-
