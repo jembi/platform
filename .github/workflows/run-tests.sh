3f2538267305b3bf0317eb449dd8944637983108
bradsawadye <bradsawadye@gmail.com>
2024-05-27 15:36:01 +0200

    Disable the clickhouse test case (temporarily)
    To debug

--- .github/workflows/run-tests.sh
+++ .github/workflows/run-tests.sh
@@ -33,7 +33,7 @@ else
         echo "$folder_name was changed"
 
         if [[ $folder_name == *"clickhouse"* ]]; then
-            DOCKER_HOST=ssh://ubuntu@$GITHUB_RUN_ID.jembi.cloud yarn test:"$NODE_MODE":clickhouse
+            # DOCKER_HOST=ssh://ubuntu@$GITHUB_RUN_ID.jembi.cloud yarn test:"$NODE_MODE":clickhouse
         elif [[ $folder_name == *"elastic"* ]] || [[ $folder_name == *"kibana"* ]] || [[ $folder_name == *"logstash"* ]]; then
             DOCKER_HOST=ssh://ubuntu@$GITHUB_RUN_ID.jembi.cloud yarn test:"$NODE_MODE":elk
         elif [[ $folder_name == *"kafka"* ]] || [[ $folder_name == *"monitoring"* ]]; then
